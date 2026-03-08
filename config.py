2cedb1a1cd6c87df695c328eea84c371452f2425
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2026-03-08 23:02:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_ID = 14580130
+API_HASH = "d1018b33de95b18c5afb321ad0dcee0b"
+BOT_TOKEN = "8631605369:AAHdQhLcl4_c_veVa6cXfjnyQH4-H33av-w"
+
+OWNER_ID = 7570694792
+
+CLICKHOUSE_HOST = "localhost"
+CLICKHOUSE_DB = "telegram"
+
+CSV_PATH = "/root/Exbot/processed/eyeofgod_telegram_774k.csv"
+
+MAX_RESULTS = 10
